98e54d54257b8c4a529598ba1b0ae4c3af0e27b7
Nadila Andika <nadilaandika1002@gmail.com>
2022-05-25 19:53:25 +0700

    memprbaiki file sql

--- database/pustaka.sql
+++ database/pustaka.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 25 Bulan Mei 2022 pada 06.34
+-- Waktu pembuatan: 25 Bulan Mei 2022 pada 14.52
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 7.4.16
 
@@ -201,15 +201,6 @@ CREATE TABLE `temp` (
   `tahun_terbit` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `temp`
---
-
-INSERT INTO `temp` (`id`, `tgl_booking`, `id_user`, `email_user`, `id_buku`, `judul_buku`, `image`, `penulis`, `penerbit`, `tahun_terbit`) VALUES
-(1, '2022-05-12 20:29:56', '25', 'Fatur@gmail.com', 1, 'Statistika dengan Program Komputer', 'img1557402455.jpg', 'Ahmad Kholiqul Amin', 'Deep Publish', 2014),
-(2, '2022-05-12 20:30:00', '25', 'Fatur@gmail.com', 2, 'Mudah Belajar Komputer untuk Anak', 'img1557402397.jpg', 'Bambang Agus Setiawan', 'Huta Media', 2014),
-(3, '2022-05-12 20:30:02', '25', 'Fatur@gmail.com', 5, 'PHP Komplet', 'img1555522493.jpg', 'Jubilee', 'Elex Media Komputindo', 2017);
-
 -- --------------------------------------------------------
 
 --
@@ -227,6 +218,26 @@ CREATE TABLE `users` (
   `is_active` int(1) NOT NULL,
   `tanggal_input` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `alamat`, `email`, `image`, `password`, `role_id`, `is_active`, `tanggal_input`) VALUES
+(1, 'Imam Nawawi', '', 'imam.imw@gmail.com', 'pro1557486527.jpg', '$2y$10$jLgqE1IBWTdVFuBfgq4u/upFdTkdRmKSigfZ7M8qHbjkTmnf26D5a', 1, 1, 1554218983),
+(3, 'Maruloh, M.Kom', '', 'maruloh.mru@bsi.ac.id', 'default.jpg', '$2y$10$beSdsua15A.A.tmiLIsmfuQCLWGdptUwjXlGI2u2kqxlpPXpXqZ72', 1, 1, 1554219788),
+(13, 'Rizkiyah', '', 'kiki.hidayah@gmail.com', 'pro1557648081.jpg', '$2y$10$237zpGsuPlkDaH0z0kMhKOcP.1pmMx8cxrUXGgh2ErFPQZ5Eb8p72', 1, 1, 1557609676),
+(14, 'Eni Irfiani', '', 'eni.enf@bsi.ac.id', 'default.jpg', '$2y$10$pVXQd4Hs4yrtWjIjhyngge2tsG5nWbL.B956XbElPOM0Q0EsSip.a', 1, 1, 1563868032),
+(15, 'Andriansah', '', 'andriansah.aiy@bsi.ac.id', 'default.jpg', '$2y$10$perspnO9EwfN24C1UnIlVuJl9WfZazMq.KynnKNcTdqprkVSfcmiC', 1, 1, 1563868080),
+(16, 'Imam Khotib', '', 'imamkhotib0@gmail.com', 'default.jpg', '$2y$10$G6Z.Bq1htczywNfgmC0Ti.WTidif/a7ztpgMOyj1r7v6UWJzDSOmW', 1, 1, 1564436443),
+(18, 'sahrul', 'jl.mawar', 'sahrul@gmail.com', 'pro1648623413.png', '$2y$10$/2ZDk0wHs.qazhgObm1Abe9ON9tZrPR4PhshPOzwHgf.4XU0WNBl6', 1, 1, 1648023296),
+(19, 'ikke kholaiyah', 'bulak teko', 'ikkekholaiyah@gmail.com', 'pro1649150076.jpg', '$2y$10$OA6rKFSvmcqEmeho1BkyTOIWdDn2aW/1QHuMod80ccMd5CMEaQfZW', 2, 1, 1649147566),
+(20, 'Yusuf', 'Jalan jalan', 'yusuf@gmail.com', 'default.jpg', '$2y$10$ilM38ds/KLLGfr9BGRlbQOuq/HRVzxY8vPAZ3Kej1W/bNE7uPj8ZW', 2, 1, 1650527760),
+(21, 'yusuf', 'Jln ', 'yusuf@gmail.com', 'default.jpg', '$2y$10$KDitkxOBpW4Ft1PkMJJjIO2nEh5rQJUqImnpFSeTcQmGwQprPDg56', 2, 1, 1650528011),
+(22, 'yusuf', 'Jln ', 'yusuf1@gamil.com', 'default.jpg', '$2y$10$lFTDXOBRxbPcoDw3MZAzhO10zEIdlwnxy0cZKb2EZCz41cTpkyATu', 2, 1, 1650528133),
+(23, 'syahrul', 'Jln ', 'sahrul@bsi.ac.id', 'default.jpg', '$2y$10$o4XmJ697Gji.tgTzG/bllOfdz45yWPVMGqqhphs4aDseGFN.YYc56', 2, 1, 1650529451),
+(24, 'admin', '', 'adminpustaka@admin.com', 'default.jpg', '$2y$10$SzWxa43H3y2f7L6R/kI8eeufnBe890eZJ20/EZq4RcaN31.0rE3ZW', 1, 1, 1651041998),
+(25, 'MUHAMAD FATHUR RASIDI', 'Jakarta', 'Fatur@gmail.com', 'pro1652362112.png', '$2y$10$.xGN/W6drSLCkcA5Z2vG3..AjgZZT0aU96MRCkCSMg5GpbkRyQGj6', 2, 1, 1652361643);
 
 --
 -- Indexes for dumped tables
@@ -331,13 +342,13 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT untuk tabel `temp`
 --
 ALTER TABLE `temp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
